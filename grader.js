f66bd67ee9d6b3f0e6715fc76c79cedbde272163
Kiky Tangerine <kwtangerine@gmail.com>
2013-07-12 02:26:15 +0000

    hw3 part 3

--- grader.js
+++ grader.js
@@ -24,9 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-
+var fromurl = "fromurl.html";
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
@@ -35,6 +36,25 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
+var checkUrl = function(url, checks) {
+    if(url) {
+	restler.get(url).on('complete', function(result) {
+	if (result instanceof Error) {
+		console.log("Unable to retrieve html page from URL %s. Exiting.", url);
+		process.exit(1);
+	} else {
+	fs.writeFileSync(fromurl, result);
+	var checkJson = checkHtmlFile(fromurl, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+	fs.unlinkSync('./' + fromurl);
+	}
+	});
+    }
+    return outJson;
+};
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -65,10 +85,18 @@ if(require.main == module) {
     program
 	.option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
 	.option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <url_link>', 'URL to html page')
 	.parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if(program.url) {
+	var checkJson = checkUrl(program.url, program.checks);
+    }
+    else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
